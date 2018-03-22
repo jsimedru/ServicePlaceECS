628fcafdda073a34f429ab7d0f66eb93ead8df10
Mitchell Langston <mlangston@protonmail.com>
2018-03-21 21:48:27 -0700

    Initial linking of Create_Account page and controller to register users.

--- SPECS_Web_Server/SPECS_Web_Server/Controllers/HomeController.cs
+++ SPECS_Web_Server/SPECS_Web_Server/Controllers/HomeController.cs
@@ -4,7 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SPECS_Web_Server.Data;
 using SPECS_Web_Server.Models;
+using SPECS_Web_Server.ViewModels;
 
 namespace SPECS_Web_Server.Controllers
 {
@@ -41,11 +43,31 @@ namespace SPECS_Web_Server.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult Create_Account()
         {
             ViewData["Message"] = "Create Your Account";
 
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create_Account(CreateUser user, string alexaID){
+            User newUser = new User{
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Phone = user.Phone,
+                Username = user.Username
+            };
+
+            using (var db = new AppDb())
+            {
+                db.Connection.OpenAsync();
+                var query = new UserQuery(db);
+                query.RegisterUserAsync(newUser, user.Password);
+            }
+            return View("~/Views/Home/Index.cshtml");
         }
 
         public IActionResult OAuth()
